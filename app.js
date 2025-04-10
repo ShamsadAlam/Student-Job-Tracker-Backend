f1348680a79fae27b8e6ed8c8cdf83c453022f53
ShamsadAlam <nobleperson7571@gmail.com>
2025-04-10 18:27:41 +0530

    cors error resolved

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ import cors from "cors";
 import jobRoutes from "./routes/JobRoutes.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://shamsad-job-tracker.vercel.app/",
+    ],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/jobs", jobRoutes);
